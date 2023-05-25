55472621e87424939297f5ab77a677060866a57c
Bartosz Dobrzelecki <bdobrzelecki@rescale.com>
2023-05-25 12:34:50 +0200

    Ported to Rescale

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# pyAnsys_MAPDL_dash
-Integrating PyAnsys with Plotly's Dash and the Dash-VTK component to build an Ansys structural analysis web application
+# README
 
-VS Code ntoebook and dash web-app python scripts described in this medium article:
-[ANSYS in a Python Web App, Part 2: Pre Processing & Solving with PyMAPDL](https://towardsdatascience.com/ansys-in-a-python-web-app-part-2-pre-processing-solving-with-pymapdl-50428c18f8e7)
+## Launching
 
-![](https://github.com/shkiefer/pyAnsys_MAPDL_dash/blob/main/img/pyAnsys_MAPDL_example_in_action.gif)
+* Zip the contents of this repository and upload as Job inputs
+* Select ANSYS Mechnical software tile
+* Select hardware (1 node max)
+
+Use the folllowing Command
+
+```bash
+export PYMAPDL_MAPDL_EXEC=`find /program/ -type f -name mapdl | head -1`
+. launch.sh
+```
---
+++ launch.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Bash script for launching Rescale App using jupyter4all hack
+
+## Apply Rescale Jupyter Notebook hack
+. ~/jupyter-venv/bin/activate
+RESCALE_PYTHON_TOKEN=`jupyter notebook list | grep token | sed --regexp-extended 's/.*token=([a-z0-9]*).*/\1/g'`
+MATCH="jupyter"
+
+function killall() {
+  proc_arry=($(ps -ef | grep $1 | awk '{print $2}'))
+
+  for i in "${proc_arry[@]}"
+  do
+    kill -9 $i
+  done
+}
+
+killall $MATCH
+deactivate
+
+## Set up desired Python environment
+PYTHON_VERSION=3.9
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p $HOME/miniconda
+eval "$(/${HOME}/miniconda/bin/conda shell.bash hook)"
+rm ~/miniconda.sh
+
+conda create -y --name RESCALE python=$PYTHON_VERSION
+conda activate RESCALE
+pip install --upgrade pip
+
+## Install dependencies and start the web server
+pip install -r requirements.txt
+gunicorn --certfile $HOME/.certs/nb.pem --keyfile $HOME/.certs/nb.key -b 0.0.0.0:8888 app:server
